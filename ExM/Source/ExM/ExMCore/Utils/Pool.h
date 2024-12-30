b231d7cbb257812ce94252730d7db9e4ed31c6ff
Moises Pirela <moisespirela96@gmail.com>
2024-12-30 10:29:06 -0300

    Untrack binaries

---
+++ ExM/Source/ExM/ExMCore/Utils/Pool.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include "ExM/ExMCore/Core/IPostProcessEvent.h"
+
+template <typename T>
+class Pool {
+	
+private:
+	TArray<T*> pool;
+	int size;
+
+public:
+	Pool(int32 InitialSize) : size(InitialSize)
+	{
+		for (int32 i = 0; i < size; ++i)
+		{
+			pool.Add(new T());
+		}
+	}
+
+	~Pool()
+	{
+		for (T* Event : pool)
+		{
+			delete Event;
+		}
+		
+		pool.Empty();
+	}
+
+	T* Allocate()
+	{
+		if (pool.Num() > 0)
+		{
+			return pool.Pop();
+		}
+		else
+		{
+			return new T();
+		}
+	}
+
+	void Deallocate(T* Event)
+	{
+		pool.Add(Event);
+	}
+};
